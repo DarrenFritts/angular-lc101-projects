222857f2b5a4678038fd68c8aa4dcfeb69d16cdd
DarrenFritts <84247704+DarrenFritts@users.noreply.github.com>
2021-08-04 09:23:16 -0500

    first commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photos to Remember';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://pbs.twimg.com/profile_images/1295809711393177606/zJcb95MI.jpg';
+  image3 = 'https://www.stlmosaicproject.org/cmss_files/attachmentlibrary/launchcode-logo-facebook1-e397a364ce733d0dee2bca438c6c1cf45d4ba9200a28c7b0b0da0e436f0ae44f_2.jpg';
 
   constructor() { }
 
